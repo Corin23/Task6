a129373bcefb1833771c97fb877c9db59d715f63
Corin23 <79444354+Corin23@users.noreply.github.com>
2022-08-05 10:37:32 +0300

    Add files via upload

--- num.c
+++ num.c
@@ -7,7 +7,7 @@
 #define BUFFERSIZE 2048
 #define FILE_IN "in.txt"
 #define FILE_OUT "out.txt"
-#define MAXNUMBERS 1000
+#define MAXNUMBERS 10000
 
 void replacestr(char *target, const char *what, const char *with) {
   char *pch;
@@ -21,19 +21,22 @@ void replacestr(char *target, const char *what, const char *with) {
 }
 
 int cmpfunc(const void *a, const void *b) {
-  return (*(long long *)a - *(long long *)b);
+  long long lla = *(long long *)a;
+  long long llb = *(long long *)b;
+  return (lla > llb ? 1 : lla == llb ? 0 : -1);
 }
 
 void dataprocessing(FILE *in, FILE *out) {
 
-  char *line = (char *)malloc(BUFFERSIZE * sizeof(char));
   long long *arr = (long long *)malloc(MAXNUMBERS * sizeof(long long) + 1);
+  char *line = (char *)malloc(BUFFERSIZE * sizeof(char));
+
   long long val;
 
   const char *rep = "123";
   const char *with = "321";
 
-  int i = 0;
+  int i = 0, lines = 0;
 
   in = fopen(FILE_IN, "r");
   out = fopen(FILE_OUT, "w");
@@ -42,13 +45,14 @@ void dataprocessing(FILE *in, FILE *out) {
 
     fgets(line, BUFFERSIZE, in);
     replacestr(line, rep, with);
+    lines++;
 
     val = atoll(line);
     arr[i++] = val;
   }
 
-  qsort(arr, MAXNUMBERS, sizeof(long long *), cmpfunc);
-  for (int i = 0; i < MAXNUMBERS; i++)
+  qsort(arr, lines, sizeof(long long *), cmpfunc);
+  for (int i = 0; i < lines; i++)
     fprintf(out, "%lld\n", arr[i]);
 
   free(line);
