2d868251b18b191f42b034866a900230583083c1
Corin23 <79444354+Corin23@users.noreply.github.com>
2022-08-05 10:09:44 +0300

    Add files via upload

--- num.c
+++ num.c
@@ -3,11 +3,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <time.h>
-#include <unistd.h>
 
 #define BUFFERSIZE 2048
-#define MAXNUMBERS 1000000
+#define FILE_IN "in.txt"
+#define FILE_OUT "out.txt"
+#define MAXNUMBERS 1000
 
 void replacestr(char *target, const char *what, const char *with) {
   char *pch;
@@ -20,92 +20,52 @@ void replacestr(char *target, const char *what, const char *with) {
   }
 }
 
-void quicksort(long long number[], int first, int last) {
-  long i, j, pivot, temp;
-  if (first < last) {
-    pivot = first;
-    i = first;
-    j = last;
-
-    while (i < j) {
-      while (number[i] <= number[pivot] && i < last)
-        i++;
-      while (number[j] > number[pivot])
-        j--;
-      if (i < j) {
-        temp = number[i];
-        number[i] = number[j];
-        number[j] = temp;
-      }
-    }
-    temp = number[pivot];
-    number[pivot] = number[j];
-    number[j] = temp;
-    quicksort(number, first, j - 1);
-    quicksort(number, j + 1, last);
-  }
+int cmpfunc(const void *a, const void *b) {
+  return (*(long long *)a - *(long long *)b);
 }
 
-void sortfile(FILE *fp) {
-  long long Arr[MAXNUMBERS];
-  int c, i, j, pass = 0;
-  FILE *fpj;
-  fpj = fopen("out.txt", "r");
-  fp = fopen("outsort.txt", "w");
+void dataprocessing(FILE *in, FILE *out) {
 
-  if (fpj == NULL) {
+  char *line = (char *)malloc(BUFFERSIZE * sizeof(char));
+  long long *arr = (long long *)malloc(MAXNUMBERS * sizeof(long long) + 1);
+  long long val;
 
-    perror("Failed: ");
-    exit(1);
-  }
-  printf("%s\n", "File oppened!");
+  const char *rep = "123";
+  const char *with = "321";
 
-  while (!feof(fpj) && (c < MAXNUMBERS)) {
-    fscanf(fpj, "%lld\n", &Arr[c++]);
-  }
-  printf("Size of Arr element: %ld\n", sizeof(Arr[1]));
-  quicksort(Arr, 0, c - 1);
-  for (int i = 0; i < c - 1; i++) {
-    printf("%lld\n", Arr[i]);
-    fprintf(fp, "%lld\n", Arr[i]);
-  }
+  int i = 0;
 
-  // c = i;
-  fclose(fp);
-  fclose(fpj);
-}
+  in = fopen(FILE_IN, "r");
+  out = fopen(FILE_OUT, "w");
 
-void replace(FILE *fin, FILE *fout) {
-  char *result;
-  char buffer[BUFFERSIZE];
-  char oldword[] = "123";
-  char newword[] = "321";
+  while (!feof(in)) {
 
-  fin = fopen("in.txt", "r");
-  fout = fopen("out.txt", "wr");
+    fgets(line, BUFFERSIZE, in);
+    replacestr(line, rep, with);
 
-  if (fin == NULL) {
-    printf("Couldn't open file in.txt!");
-    exit(1);
-  }
-  while (!feof(fin)) {
-    fgets(buffer, BUFFERSIZE, fin);
-    replacestr(buffer, oldword, newword);
-    fprintf(fout, "%s", buffer);
+    val = atoll(line);
+    arr[i++] = val;
   }
 
-  fclose(fin);
-  fclose(fout);
+  qsort(arr, MAXNUMBERS, sizeof(long long *), cmpfunc);
+  for (int i = 0; i < MAXNUMBERS; i++)
+    fprintf(out, "%lld\n", arr[i]);
+
+  free(line);
+  free(arr);
+
+  fclose(in);
+  fclose(out);
 }
 
 int main() {
-  double time_spent = 0.0;
-  FILE *fpi, *fpo, *fpos;
-  clock_t begin = clock();
-  replace(fpi, fpo);
-  sortfile(fpos);
-  clock_t end = clock();
-  time_spent += (double)(end - begin) / CLOCKS_PER_SEC;
-  printf("Time elapsed %f", time_spent);
+  FILE *fin, *fout;
+  fin = fopen(FILE_IN, "r");
+  fout = fopen(FILE_OUT, "w");
+
+  dataprocessing(fin, fout);
+
+  fclose(fin);
+  fclose(fout);
   return 0;
 }
